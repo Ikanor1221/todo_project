14e00e099e3da2b746d516efc8f75746a026c00f
your_github_username <your_github_email>
2023-03-12 16:38:55 +0200

    Properly implement emerging layered and mobile menues

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ const projectMenu = document.querySelector("#projectMenu1");
 menuModify.addEventListener("click", (e) => {
     projectMenu.classList.toggle("hidden");
 })
+
+const menuControl = document.querySelector("#menu_control");
+const mainMenu = document.querySelector("#menu_main_bar");
+const mainScreen = document.querySelector("#sorting");
+
+menuControl.addEventListener("click", (e) => {
+    mainMenu.classList.toggle("removed");
+    mainScreen.classList.toggle("grid_auto");
+})
+
