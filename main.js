50c8cdafcfce8cdd100e8dc81d849b68ad04a6d0
your_github_username <your_github_email>
2023-03-12 19:08:27 +0200

    Implement basic menue showimg controls

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const taskModify = document.querySelector("#buttonModify1");
 const taskMenu = document.querySelector("#taskMenu1");
 
-taskModify.addEventListener("click", (e) => {
-    taskMenu.classList.toggle("hidden");
-})
-
 const menuModify = document.querySelector("#buttonProjectMenu1");
 const projectMenu = document.querySelector("#projectMenu1");
 
-menuModify.addEventListener("click", (e) => {
-    projectMenu.classList.toggle("hidden");
-    
+const bodyElement = document.querySelector("body");
+
+bodyElement.addEventListener("click", (e) => {
+    console.log(e.target)
+    if (!e.target.closest("#buttonModify1") || !taskMenu.classList.contains("hidden")) {
+        taskMenu.classList.add("hidden");
+    } else {
+         taskMenu.classList.remove("hidden");
+    }
+
+    if (!e.target.closest("#buttonProjectMenu1") || !projectMenu.classList.contains("hidden")) {
+        projectMenu.classList.add("hidden");
+    }  else {
+        projectMenu.classList.remove("hidden");
+    }
 })
 
 const menuControl = document.querySelector("#menu_control");
